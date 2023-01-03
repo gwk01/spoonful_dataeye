dffc71a57ca38bdd25263ed9445d9bcf1cfc630d
gwk01 <78788407+gwk01@users.noreply.github.com>
2023-01-03 15:48:45 +0200

    Update spoonful.py

--- spoonful.py
+++ spoonful.py
@@ -674,7 +674,7 @@ fig.add_scattergeo(
 
 with col3:
     st.markdown(f'<p style="text-align:center; color:#FFFFFF;font-family:Arial;font-size:18px;border-radius:0%;">Spoonful Subsidy Food Basket Price (USD):</p>', unsafe_allow_html=True)
-    fig.update_layout (plot_bgcolor='Light Grey', showlegend=False,title_font_family="Arial",title_font_size=20)
+    fig.update_layout (plot_bgcolor='Light Grey', showlegend=False)
     fig.update_layout(title={
         'text': "Lebanese Districts Poverty Predictions in 2022  <br> by Deploying Satellite Imagery Data on Random Forest Regressor",
         'y':0.9,
